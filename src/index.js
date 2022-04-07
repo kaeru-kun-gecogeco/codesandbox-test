a00efe07189442a9709ade1ad39b92b90264ec19
kaeru-kun-gecogeco <75885071+kaeru-kun-gecogeco@users.noreply.github.com>
2022-04-07 23:31:45 +0900

    追加ボタンのイベントを作成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 import "./styles.css";
+
+const onClickAddChk = () => {
+  const inputText = document.getElementById("add-text").value;
+  console.log(inputText);
+  if (inputText.trim() === "") {
+    document.getElementById("add-text").className = "error-text";
+    alert("入力してください");
+  } else {
+    document.getElementById("add-text").className = "";
+    onClickAdd(inputText);
+  }
+};
+
+const onClickAdd = (inputText) => {
+  document.getElementById("add-text").value = "";
+
+  // <li>タグ生成
+  const li = document.createElement("li");
+
+  // <div>タグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // <p>タグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // 完了ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // 削除ボタンを生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // <li>タグの中身に子要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("imcomplete-list").appendChild(li);
+
+  console.log(inputText);
+  console.log(li);
+};
+
+// 追加ボタンのイベントを設定
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAddChk());
